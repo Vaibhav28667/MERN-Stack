efed0f344a10c2fe30487707ebf9ee70285ea307
vaibhav28667 <vaibhavsingh1278@gmail.com>
2026-02-11 13:52:22 +0530

    addition of footer and starting of javascript

---
+++ SECTION 1/javascript/condition.js
@@ -0,0 +1,16 @@
+num = 20;
+
+if(num%2 == 0){
+    console.log("even");
+
+     msg='hello';
+
+    const msg2='world';
+
+    // msg2='hello world';
+
+}else{
+    console.log("odd");
+}
+
+console.log(msg);
---
+++ SECTION 1/javascript/index.js
@@ -0,0 +1,17 @@
+console.log("Hello javascript!");
+
+a = 10;
+
+console.log(a);
+
+myname = "John Doe";
+
+console.log(myname);
+
+ishungry = true;
+
+console.log(ishungry);
+
+console.log(typeof a);
+console.log(typeof myname);
+console.log(typeof ishungry);
