f14066d64877016ebf650ab68b99acfe1f9a6e61
vaibhav28667 <vaibhavsingh1278@gmail.com>
2026-02-12 00:14:17 +0530

    making event handling page

--- SECTION 1/javascript/loops.js
+++ SECTION 1/javascript/loops.js
@@ -21,3 +21,10 @@ do {
     console.log(k);
     k++;
 } while (k > 10);
+
+// JavaScript practice questions
+// 1. WAP to check if a number is prime.
+// 2. WAP to check if a person is eligible for DL.
+// 3. WAP to find greatest of 3 numbers.
+// 4. WAP to check if a number is divisible by 7 and 11.
+// 5. WAP to check if a number is palindrome.
