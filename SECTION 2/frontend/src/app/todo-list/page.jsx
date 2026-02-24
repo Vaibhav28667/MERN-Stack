82239da863abd25b8b905d523afe8c5be9c1ee07
vaibhav28667 <vaibhavsingh1278@gmail.com>
2026-02-24 13:57:24 +0530

    feat: Add lucide-react dependency and implement task management in TodoList component

--- SECTION 2/frontend/src/app/todo-list/page.jsx
+++ SECTION 2/frontend/src/app/todo-list/page.jsx
@@ -1,14 +1,37 @@
 'use client'
+import { Trash2 } from 'lucide-react';
 import React, { useState } from 'react'
 
 const TodoList = () => {
 
     // let count = 1;
 
-    const [count, setCount] = useState(1);
+    // const [count, setCount] = useState(1);
 
+    const [TaskList, setTaskList] = useState([
+        
+    ]);
+
+    const addNewTask = (e) => {
+        if(e.code === 'Enter'){
+            console.log(e.target.value);
+            
+            const newTask = { task: e.target.value, completed: false};
+
+            setTaskList([ newTask, ...TaskList ])
+
+            e.target.value = '';
+        }   
+    };
+
+
+    const deleteTask = (index) => {
+        const temp = TaskList;
+        temp.splice(index, 1);
+        setTaskList([...temp]);
+    }
   return (
-    <div className='bg-gray-100 h-screen'>
+    <div className='bg-gray-100 min-h-screen'>
 
         {/* <button className='border p-4 ' onClick={() => {alert('Button was Clicked'); }}>
             Submit
@@ -31,14 +54,35 @@ const TodoList = () => {
             <div className='border rounded-xl bg-white shadow-xl'>
                 <div className='p-5'>
                         <input type='text'
+                            onKeyDown={addNewTask}
                             className='py-2 px-5 text-lg w-full rounded-md bg-blue-100'
                             placeholder='Enter your text'
                             />
                 </div>
-                <div>
-                    <p className='text-center font-bold text-gray-400 text-xl'>
-                        😎 No Todos to show yet
-                    </p>
+                <div className='p-5'>
+
+                    {
+                        TaskList.length === 0 ? (
+                            <p className='text-center font-bold text-gray-400 text-xl'>
+                                 😎 No Todos to show yet
+                            </p>
+                          ):(
+                            TaskList.map((obj, index)=>{
+                                return <div key= {index}
+                                className='border border-gray-400 p-4 rounded-lg mb-4 flex justify-between items-center'>
+                                    
+                                    
+                                    <p className='text-lg'>{obj.task}</p>
+                                    <button
+                                        onClick={ () => {deleteTask(indes)}}
+                                        className='bg-red-500 text-white p-2 rounded-md '>
+                                        <Trash2 />
+                                    </button>
+                                    </div>
+                            })
+                          )
+                    
+                    }
                 </div>
             </div>
         </div>
